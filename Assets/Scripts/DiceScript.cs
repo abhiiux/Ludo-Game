b5c323fba7d01a9f3248e8b42d2ca09f6b0d170e
Abhidev ramesh <abhidevramesh@gmail.com>
2025-06-23 14:38:10 +0530

    Frist commit

---
+++ Assets/Scripts/DiceScript.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class DiceScript : MonoBehaviour
+{
+    [SerializeField] GameObject diceAnimation;
+    [SerializeField] GameObject diceObjects;
+    [SerializeField] bool isLog;
+    [SerializeField] int rollValue;
+
+    int lastValue;
+    SpriteRenderer[] diceSprites;
+    public Action<int> DiceRoll;
+
+    void Start()
+    {
+        diceSprites = diceObjects.GetComponentsInChildren<SpriteRenderer>();
+
+        foreach (var item in diceSprites)
+        {
+            item.enabled = false;
+        }
+    }
+
+    public void StartDice()
+    {
+        StartCoroutine(RollDice());
+    }
+
+    private IEnumerator RollDice()
+    {
+        diceAnimation.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+
+        diceAnimation.SetActive(false);
+
+        // rollValue = UnityEngine.Random.Range(1, 7);
+        Log($"dice rolled :{rollValue}");
+        ShowDice(rollValue - 1);
+        DiceRoll?.Invoke(rollValue);
+    }
+
+    private void ShowDice(int value)
+    {
+        for (int i = 0; i < diceSprites.Length; i++)
+        {
+            diceSprites[i].enabled = (i == value);
+        }
+    }
+    private void Log(string message)
+    {
+        if (isLog)
+        {
+            Debug.Log(message);
+        }
+    }
+}
